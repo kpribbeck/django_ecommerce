fb3fb367ec500c0ddca5449be087a97d3e441d4e
Klaus <kpribbeck@gmail.com>
2026-03-12 21:17:41 -0300

    First views and templates

--- store/models.py
+++ store/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+import datetime
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+    created_at = models.DateField(default=datetime.date.today)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -14,6 +17,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateField(default=datetime.date.today)
+    updated_at = models.DateField(auto_now=True)
     
     def __str__(self):
         return self.name
@@ -26,6 +31,8 @@ class ProductVariant(models.Model):
     sku = models.IntegerField()
     price_modifier = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    created_at = models.DateField(default=datetime.date.today)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.product.name + "_" + str(self.id)
--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "store"
 urlpatterns = [
     path("", views.index, name = "index"),
+    path("product/<int:product_id>", views.getProduct, name = "product"),
+    path("product/variant/<int:productVariant_id>", views.getVariant, name="getVariant"),
 ]
